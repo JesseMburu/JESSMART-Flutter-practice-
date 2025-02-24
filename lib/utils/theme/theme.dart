a12a2a7ade16ec5fb198e3dd7860eed9638760b8
JesseMburu <jesse.mburu@strathmore.edu>
2025-02-24 10:05:39 +0300

    custom themes

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:jess_mart/utils/theme/custom_themes/text_theme.dart';
 
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/bottom_sheet_theme.dart';
+import 'custom_themes/checkbox_theme.dart';
+import 'custom_themes/chip_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
+import 'custom_themes/outlined_button_theme.dart';
+import 'custom_themes/text_field_theme.dart';
+
 class JAppTheme {
   JAppTheme._();
 
 
+  ///LIGHT THEME
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -12,8 +21,16 @@ class JAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: JTextTheme.lightTextTheme,
-    elevatedButtonTheme: ElevatedButtonThemeData()
+    chipTheme: JChipTheme.lightChipTheme,
+    appBarTheme: JAppBarTheme.lightAppBarTheme,
+    checkboxTheme: JCheckBoxTheme.lightCheckboxTheme,
+    bottomSheetTheme: JBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: JElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: JOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: JTextFormFieldTheme.lightInputDecorationTheme,
   );
+
+  ///DARK THEME
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -21,5 +38,12 @@ class JAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: JTextTheme.darkTextTheme,
+    chipTheme: JChipTheme.darkChipTheme,
+    appBarTheme: JAppBarTheme.darkAppBarTheme,
+    checkboxTheme: JCheckBoxTheme.darkCheckboxTheme,
+    bottomSheetTheme: JBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: JElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: JOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: JTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
